06ebe616c37f81455f0285bec7b0393a533876e5
DingoMC <95879619+DingoMC@users.noreply.github.com>
2022-11-07 18:57:48 +0100

    Update Z4.swift

--- Lab5/Z4.swift
+++ Lab5/Z4.swift
@@ -3,7 +3,7 @@ import Foundation
 let L1 : String = readLine()!
 var p : Bool = true
 
-for i in 0...(1 + L1.count/2) {
+for i in 0...(L1.count/2) {
     if (L1[L1.index(L1.startIndex, offsetBy: i)] != L1[L1.index(L1.endIndex, offsetBy: -i-1)]) {
         p = false
         print("To nie jest palindrom")
