55ee571138ee41416ff76525ad9144f6dc266135
DingoMC <95879619+DingoMC@users.noreply.github.com>
2022-11-14 15:33:38 +0100

    Delete L2-Z1.swift

--- L2-Z1.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-let now : Int = 2022
-var rok : Int = 0
-print("Podaj rok urodzenia: ")
-if let tmp = Int(readLine()!) {
-    rok = tmp
-}
-print("Masz " + String(now - rok) + " lat")
