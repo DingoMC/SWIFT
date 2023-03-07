cb712014e714cfff10cfa0faad850e3e854f08eb
DingoMC <95879619+DingoMC@users.noreply.github.com>
2023-03-07 09:55:18 +0100

    Update Z3.swift

--- Lab_UI_2/Z3.swift
+++ Lab_UI_2/Z3.swift
@@ -6,18 +6,18 @@ struct ContentView: View {
     @State var b3: Int = 0
     @State var wynik: String = ""
     var body: some View {
-        VStack {
+        VStack(alignment: .center) {
             HStack {
                 Text("Podaj Bok 1:")
-                TextField("0", text: Binding(get: {String(b1)}, set:{b1 = Int($0) ?? 0}))
+                TextField("0", text: Binding(get: {String(b1)}, set:{b1 = Int($0) ?? 0})).frame(width: 60).border(.black)
             }
             HStack {
                 Text("Podaj Bok 2:")
-                TextField("0", text: Binding(get: {String(b2)}, set:{b2 = Int($0) ?? 0}))
+                TextField("0", text: Binding(get: {String(b2)}, set:{b2 = Int($0) ?? 0})).frame(width: 60).border(.black)
             }
             HStack {
                 Text("Podaj Bok 3:")
-                TextField("0", text: Binding(get: {String(b3)}, set:{b3 = Int($0) ?? 0}))
+                TextField("0", text: Binding(get: {String(b3)}, set:{b3 = Int($0) ?? 0})).frame(width: 60).border(.black)
             }
             Button(action: {
                 if b1 + b2 > b3 && b2 + b3 > b1 && b1 + b3 > b2 {
@@ -27,6 +27,7 @@ struct ContentView: View {
                     wynik = "Nie da sie zbudowac trojkata"
                 }
             }, label: {Text("Trojkat")})
+            Text("Podano \(b1), \(b2), \(b3)")
             Text("\(wynik)").foregroundColor(wynik.contains("Nie") ? .red : .green)
         }
         .padding()
