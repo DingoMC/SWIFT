09a7c3aa8406e9cb8a0674a8c621464973b4f6e1
DingoMC <95879619+DingoMC@users.noreply.github.com>
2023-03-07 08:25:18 +0100

    Update Z1.swift

--- Lab_UI_1/Z1.swift
+++ Lab_UI_1/Z1.swift
@@ -7,8 +7,8 @@ struct ContentView: View {
     let nazwy : [String] = ["poniedzialek", "wtorek", "sroda", "czwartek", "piatek", "sobota", "niedziela"]
     var body: some View {
         VStack {
-            Text("Podaj wybrany dzien tygodnia").foregroundColor(.red)
-            TextField("Wpisz dzien tygodnia", text: $dzien)
+            Text("Podaj wybrany dzien tygodnia").foregroundColor(.red).rotation3DEffect(.degrees(45.0), axis: (x: 1, y: 0, z: 0))
+            TextField("Wpisz dzien tygodnia", text: $dzien).multilineTextAlignment(.center)
             Button(action: {
                 dzienWybor=dzien
                 if nazwy.contains(dzien.lowercased()) {
@@ -19,7 +19,7 @@ struct ContentView: View {
                 }
             }, label: {Text("Zatwierdz wybor")}).background(Color.green)
             Text("Wybrano: \(dzienWybor)")
-            Text("\(dzienOK)")
+            Text("\(dzienOK)").foregroundColor(dzienOK.contains("Prawidlowy") ? .green : .red)
         }
         .padding()
     }
